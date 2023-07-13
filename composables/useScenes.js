86b3085f09c77a5932ac6dacfbd2e235770cd756
Filip Zindovic <filip@totally.group>
2023-07-13 10:26:24 +0100

    Fixed lint errors

--- composables/useScenes.js
+++ composables/useScenes.js
@@ -1,29 +1,29 @@
-const config = useRuntimeConfig()
+const config = useRuntimeConfig();
 
 export const useScenes = () => {
-  const scenesData = useState("scenesData", () => [])
-  const scenesDataIsLoading = ref(false)
+  const scenesData = useState('scenesData', () => []);
+  const scenesDataIsLoading = ref(false);
   const fetchScenesData = async () => {
     try {
-      scenesDataIsLoading.value = true
+      scenesDataIsLoading.value = true;
 
       const data = await $fetch(config.public.SCENES_BASE_URL);
 
       if (data.length) {
-        const formattedData = JSON.parse(data)
-        scenesData.value = formattedData
+        const formattedData = JSON.parse(data);
+        scenesData.value = formattedData;
       }
 
-      scenesDataIsLoading.value = false
+      scenesDataIsLoading.value = false;
     } catch (error) {
-      scenesDataIsLoading.value = false
+      scenesDataIsLoading.value = false;
       console.error(error);
     }
-  }
+  };
 
   return {
     fetchScenesData,
     scenesDataIsLoading,
     scenesData
-  }
-}
+  };
+};

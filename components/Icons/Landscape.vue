2b996324c1f987076a1b3647cb7d3284f5adaff6
Filip Zindovic <filip@totally.group>
2023-07-13 09:23:51 +0100

    Added icons as components to project

---
+++ components/Icons/Landscape.vue
@@ -0,0 +1,7 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="11" viewBox="0 0 20 11" fill="none">
+    <path d="M0 11L5.45455 3.66667L9.54545 9.16667H16.3636L11.8182 3.07083L9.54545 6.09583L8.40909 4.58333L11.8182 0L20 11H0ZM3.63636 9.16667H7.27273L5.45455 6.71458L3.63636 9.16667Z" fill="currentColor"/>
+  </svg>
+</template>
+
+

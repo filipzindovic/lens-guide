b5e99eb739b7b4d39094d47b4d193bc9144e56fe
Filip Zindovic <filip@totally.group>
2023-07-13 09:37:31 +0100

    Added BaseLoader component

---
+++ components/Base/Loader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="BaseLoader h-w-full flex-center-hv text-center">
+    <IconsSpinner class="BaseLoader__icon" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.BaseLoader {
+  &__icon {
+    animation: loading-spinner 1s linear infinite;
+
+    @keyframes loading-spinner {
+      from {
+        transform: rotate(0deg);
+      }
+      to {
+        transform: rotate(360deg);
+      }
+    }
+  }
+}
+</style>

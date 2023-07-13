65fb84f8af209b0f20589bbffe3390d77c8d0a16
Filip Zindovic <filip@totally.group>
2023-07-13 09:21:51 +0100

    Added index.vue to pages and removed app.vue

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <button class="mt-2 ml-2 px-4 py-2 font-semibold bg-cyan-500 text-white rounded-full shadow-sm">
+      Show Lens guide
+    </button>
+  </div>
+</template>

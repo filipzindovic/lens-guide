3b80467ad1cc2d26306460527f8ee04e11443ddb
Filip Zindovic <filip@totally.group>
2023-07-13 09:14:30 +0100

    Added tailwind and global scss file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,16 @@ export default defineNuxtConfig({
   // devtools: { enabled: true },
   ssr: false,
   modules: [
-    '@nuxtjs/eslint-module'
-  ]
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/tailwindcss'
+  ],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/assets/scss/_variables.scss";'
+        }
+      }
+    }
+  }
 });
